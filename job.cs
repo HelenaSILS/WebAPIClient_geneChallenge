46787208823cd72687da058346bf924fce154b29
Helena Schubert I. Lima Silva <helena.sils@gmail.com>
2021-08-21 14:33:02 -0300

    job class updated

--- job.cs
+++ job.cs
@@ -1,25 +1,19 @@
 using System;
-using System.Text.Json.Serialization;
 
 namespace WebAPIClient
 {
     public class Job
     {
-        [JsonPropertyName("id")]
         public string Id {get; set;}
 
         public string Path{get;set;}
 
-        [JsonPropertyName("type")]
         public string Type {get; set;}
 
-        [JsonPropertyName("strand")]
         public string Strand {get; set;}
 
-        [JsonPropertyName("strandEncoded")]
         public string StrandEncoded {get; set;}
 
-        [JsonPropertyName("geneEncoded")]
         public string GeneEncoded {get; set;}
 
         public Job(){
